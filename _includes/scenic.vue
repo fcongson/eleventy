ba9afb24520d26839c92dc4f978120489fb06aa1
Frank Congson <fcongson@gmail.com>
2020-12-17 14:03:40 +1300

    Another vue component

---
+++ _includes/scenic.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <h1>🏔️</h1>
+    <p>Enjoy the view!</p>
+  </main>
+</template>
+<style>
+main {
+  font-family: monospace;
+}
+</style>
--- index.vue
+++ index.vue
@@ -1,14 +1,19 @@
 <template>
-  <room-with-a />
+  <div>
+    <room-with-a />
+    <scenic />
+  </div>
 </template>
 <script>
 import roomWithA from "./_includes/room-with-a.vue";
+import scenic from "./_includes/scenic.vue";
 export default {
   data: {
     layout: "layout.11ty.js",
   },
   components: {
     roomWithA,
+    scenic,
   },
 };
 </script>
